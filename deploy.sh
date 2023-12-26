4f968dd683df0633441ba10f769e6c54929f562e
nsannikanti <125451420+nsannikanti@users.noreply.github.com>
2023-12-26 11:45:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ if [ -d "hitcount-app" ]; then
   echo "hitcount-app does exist."
   rm -rf hitcount-app
 fi
-git clone https://github.com/socgenaws/hitcount-app.git
+git clone https://github.com/nsannikanti/hitcount-app.git
 cd hitcount-app
 docker-compose stop
 docker-compose rm 
